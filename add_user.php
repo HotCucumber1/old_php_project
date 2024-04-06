277178190ba719b32fcf7445d24059fdb381a80b
hotcucumber <shadowpersik05@mail.ru>
2024-04-06 07:53:21 +0300

    UserTable model (no controller)

--- add_user.php
+++ add_user.php
@@ -1,14 +1,49 @@
 <?php
-use controller\UserController;
+/*use controller\UserController;
 require_once './src/controller/UserController.php';
-require './data/exceptions/DataBaseException.php';
+require './data/exceptions/DataBaseException.php';*/
+require_once __DIR__ . '/src/model/User.php';
+require_once __DIR__ . '/src/model/UserTable.php';
+require_once 'connection.php';
+require_once __DIR__ . '/data/exceptions/DataBaseException.php';
+
 
 
 try {
+    $user = new User(
+        null,
+        $_POST['name'],
+        $_POST['last_name'],
+        $_POST['middle_name'],
+        $_POST['gender'],
+        $_POST['birth_date'],
+        $_POST['email'],
+        $_POST['phone'],
+        $_POST['avatar_path']
+    );
+
+    $connectionParams = getConnectionParams();
+    $connection = connectDatabase($connectionParams);
+
+    $table = new UserTable($connection);
+    $last = $table->addUser($user);
+
+    if ($last) {
+        $redirectUrl = "/show_user.php?user_id={$last}";
+        header('Location: ' . $redirectUrl, true, 303);
+        die();
+    }
+}
+catch (DataBaseException $exception) {
+    echo "ERROR: " . $exception->getMessage();
+}
+
+
+/*try {
     $controller = new UserController();
     $controller->addNewUser($_POST);
 
 }
 catch (DataBaseException $exception) {
      echo "ERROR: " . $exception->getMessage();
-}
+}*/
--- show_user.php
+++ show_user.php
@@ -1,8 +1,35 @@
 <?php
-use controller\UserController;
+/*use controller\UserController;
 require_once './src/controller/UserController.php';
-require './data/exceptions/DataBaseException.php';
+require './data/exceptions/DataBaseException.php';*/
+require_once __DIR__ . '/src/model/User.php';
+require_once __DIR__ . '/src/model/UserTable.php';
+include 'connection.php';
+include __DIR__ . '/data/exceptions/DataBaseException.php';
 
+
+try {
+    $connectionParams = getConnectionParams();
+    $connection = connectDatabase($connectionParams);
+    $table = new UserTable($connection);
+    $user = $table->findUser($_GET['user_id']);
+
+    echo htmlentities($user->getUserId()) . "<br>";
+    echo htmlentities($user->getFirstName()) . "<br>";
+    echo htmlentities($user->getLastName()) . "<br>";
+    echo htmlentities($user->getMiddleName()) . "<br>";
+    echo htmlentities($user->getGender()) . "<br>";
+    echo htmlentities($user->getBirthDate()) . "<br>";
+    echo htmlentities($user->getEmail()) . "<br>";
+    echo htmlentities($user->getPhone()) . "<br>";
+    echo htmlentities($user->getAvatarPath()) . "<br>";
+}
+catch (DataBaseException $exception) {
+    echo "ERROR: " . $exception->getMessage();
+}
+
+
+/*
 try {
     $controller = new UserController();
     $controller->showUser($_GET);
@@ -11,3 +38,4 @@ try {
 catch (DataBaseException $exception) {
     echo "ERROR: " . $exception->getMessage();
 }
+*/
